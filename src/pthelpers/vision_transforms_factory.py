28a249836956f07b3a752b9a16d2f58d2f5ac10a
Johannes <zelger.johannes@gmail.com>
2022-07-26 20:10:43 +0100

    fixed transformers

--- src/pthelpers/vision_transforms_factory.py
+++ src/pthelpers/vision_transforms_factory.py
@@ -4,39 +4,51 @@ from torchvision.transforms import transforms
 vision_transforms_factory_ingredient = Ingredient("transforms_factory")
 
 
-@vision_transforms_factory_ingredient.config
-def cfg():
-    resize: None
-    rotate = None
-    color_jitter: None
-    perspective: None
-    force_3ch = False
+# @vision_transforms_factory_ingredient.config
+# def cfg():
+#     resize: {
+#         "width": None,
+#         "height": None
+#     }
+#     rotate = None
+#     color_jitter: {
+#         "brightness": None,
+#         "hue": None,
+#         "contrast": None,
+#         "saturation": None
+#     }
+#     perspective: {
+#         "strength": None,
+#         "probability": None
+#     }
+#     force_3ch = False
 
 
 @vision_transforms_factory_ingredient.capture
 def generate_train_transforms(_config):
     trafos = []
-    if _config["resize"]:
+
+    if "resize" in _config and _config["resize"]:
         trafos.append(transforms.Resize((_config["resize"]["width"], _config["resize"]["height"])))
 
-    if _config["rotate"]:
+    if "rotate" in _config and _config["rotate"]:
         trafos.append(transforms.RandomRotation(_config["rotate"]))
 
-    if _config["color_jitter"]:
+    if "color_jitter" in _config and _config["color_jitter"]:
         trafos.append(transforms.ColorJitter(
-                _config["color_jitter"]["brightness"] if _config["color_jitter"]["brightness"] else 0,
-                _config["color_jitter"]["contrast"] if _config["color_jitter"]["contrast"] else 0,
-                _config["color_jitter"]["saturation"] if _config["color_jitter"]["saturation"] else 0,
-                _config["color_jitter"]["hue"] if _config["color_jitter"]["hue"] else 0))
+                _config["color_jitter"]["brightness"] if "brightness" in _config["color_jitter"] else 0,
+                _config["color_jitter"]["contrast"] if "contrast" in _config["color_jitter"] else 0,
+                _config["color_jitter"]["saturation"] if "saturation" in _config["color_jitter"] else 0,
+                _config["color_jitter"]["hue"] if "hue" in _config["color_jitter"] else 0))
 
-    if _config["perspective"]:
+    if "perspective" in _config and _config["perspective"]:
         trafos.append(transforms.RandomPerspective(
-                _config["perspective"]["strength"] if _config["perspective"]["strength"] else 0,
-                _config["perspective"]["probability"] if _config["perspective"]["probability"] else 0))
+                _config["perspective"]["strength"] if "strength" in _config["perspective"] else 0,
+                _config["perspective"]["probability"] if "probability" in _config["perspective"] else 0))
 
     trafos.append(transforms.ToTensor())
 
-    if _config["force_3ch"]:
+    if "force_3ch" in _config and _config["force_3ch"]:
         trafos.append(transforms.Lambda(lambda x: x.repeat(3, 1, 1) if x.shape[0] == 1 else x))
 
     return transforms.Compose(trafos)
