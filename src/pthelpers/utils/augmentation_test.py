4b50a208e2311f8f87fb5149de16a422263b662c
zelger.johannes@gmail.com <zelger.johannes@gmail.com>
2022-08-02 13:49:37 +0100

    added aug tester

---
+++ src/pthelpers/utils/augmentation_test.py
@@ -0,0 +1,70 @@
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torchvision
+from torchvision.transforms import ToPILImage
+
+try:
+    from pthelpers.utils.vision_transforms_factory import vision_transforms_factory_ingredient, generate_train_transforms
+except:
+    from src.pthelpers.utils.vision_transforms_factory import vision_transforms_factory_ingredient, generate_train_transforms
+
+from sacred import Experiment
+from torch.utils.data import DataLoader
+
+plt.rcParams["savefig.bbox"] = 'tight'
+torch.manual_seed(0)
+
+aug_experiment = Experiment("aug_test", ingredients=[vision_transforms_factory_ingredient])
+
+
+@aug_experiment.main
+def show_transforms(dataloader: DataLoader, num_originals: int = 1, num_augs: int = 3):
+    trafos = generate_train_transforms()
+    for i, (x, y) in enumerate(dataloader):
+        if i == num_originals: break
+        aug = [ToPILImage()(trafos(x)) for _ in range(num_augs)]
+
+        __plot__(x, aug)
+
+
+def __plot__(orig, imgs, with_orig=True, row_title=None, **imshow_kwargs):
+    if (with_orig): imgs = [orig] + imgs
+
+    num_cols = math.sqrt(len(imgs))
+    num_cols = num_cols if num_cols % 1 == 0 else math.floor(num_cols) + 1
+    num_cols = int(num_cols)
+    num_rows = num_cols
+
+    fig, axs = plt.subplots(nrows=num_rows, ncols=num_cols, squeeze=False)
+    for row_idx in range(num_rows):
+        for col_idx in range(num_cols):
+            ax = axs[row_idx, col_idx]
+            ax.imshow(np.asarray(imgs[row_idx * num_cols + col_idx]), **imshow_kwargs)
+            ax.set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+
+    if with_orig:
+        axs[0, 0].set(title='Original image')
+        axs[0, 0].title.set_size(8)
+    if row_title is not None:
+        for row_idx in range(num_rows):
+            axs[row_idx, 0].set(ylabel=row_title[row_idx])
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    vision_transforms_factory_ingredient.add_config({
+        "resize": {
+            "width": 200,
+            "height": 200
+        },
+        "rotate": 90
+    })
+
+    data = torchvision.datasets.MNIST("./data/", download=True)
+    aug_experiment.add_config({"dataloader": data})
+    aug_experiment.run()
