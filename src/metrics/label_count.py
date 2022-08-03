defd7bc0c99160f3a666208335c80a06c93e845c
zelger.johannes@gmail.com <zelger.johannes@gmail.com>
2022-08-03 17:56:48 +0100

    added speedup option from torchmetrics 0.9

--- src/metrics/label_count.py
+++ src/metrics/label_count.py
@@ -3,9 +3,10 @@ from torch import Tensor
 from torchmetrics import Metric, Accuracy
 
 Accuracy()
-
-
 class LabelCount(Metric):
+    full_state_update: bool = False
+
+
     def __init__(self, label_value: int):
         super().__init__()
         self.label_value = label_value
@@ -34,6 +35,13 @@ class LabelCount(Metric):
             raise ValueError("unknown dimension of targets, use 1D for numbers and 2D for categorical data")
 
 
+
     def compute(self) -> Tensor:
         """Computes accuracy based on inputs passed in to ``update`` previously."""
         return self.count
+
+
+if __name__ == '__main__':
+    from torchmetrics.utilities import check_forward_full_state_property
+
+    check_forward_full_state_property(LabelCount, {"label_value": 0}, {"preds": [], "target": torch.tensor([0, 0, 1, 1, 0])})
