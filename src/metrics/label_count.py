b1b366909b1488f04b70a3ba82cfa36168767f2c
zelger.johannes@gmail.com <zelger.johannes@gmail.com>
2022-08-03 17:46:13 +0100

    added label count metric

---
+++ src/metrics/label_count.py
@@ -0,0 +1,39 @@
+import torch
+from torch import Tensor
+from torchmetrics import Metric, Accuracy
+
+Accuracy()
+
+
+class LabelCount(Metric):
+    def __init__(self, label_value: int):
+        super().__init__()
+        self.label_value = label_value
+        self.add_state("count", default=torch.zeros(1), dist_reduce_fx='sum')
+
+
+    def update(self, preds: Tensor, target: Tensor) -> None:  # type: ignore
+        """Update state with predictions and targets. See
+        :ref:`references/modules:input types` for more information on input
+        types.
+        Args:
+            preds: Predictions from model (logits, probabilities, or labels)
+            target: Ground truth labels
+        """
+        if target.dim() == 1:
+            bincounts = torch.bincount(target)
+            if len(bincounts) - 1 < self.label_value:
+                return
+            self.count += bincounts[self.label_value]
+        elif target.dim() == 2:
+            for row in target:
+                if len(row) - 1 < self.label_value:
+                    return
+                self.count += row[self.label_value]
+        else:
+            raise ValueError("unknown dimension of targets, use 1D for numbers and 2D for categorical data")
+
+
+    def compute(self) -> Tensor:
+        """Computes accuracy based on inputs passed in to ``update`` previously."""
+        return self.count
