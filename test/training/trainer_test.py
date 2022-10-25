fe7cb122ae749a46a10dfcb0dded4e72e9d896a3
zelger.johannes@gmail.com <zelger.johannes@gmail.com>
2022-10-25 09:20:13 +0100

    finished second test

--- test/training/trainer_test.py
+++ test/training/trainer_test.py
@@ -6,9 +6,7 @@ from unittest.mock import MagicMock
 
 import torch
 import wandb
-from torch import optim
 from torch.optim import Adam
-from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader
 from torchmetrics import Accuracy
 from torchvision import transforms, datasets
@@ -23,24 +21,27 @@ class Test(unittest.TestCase):
         wandb.init(mode="disabled")
         wandb.config.update({"cp_base_path": "checkpoints"})
 
-        transform=transforms.Compose([
+        transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,))
-            ])
+        ])
         train_data = datasets.MNIST('../data', train=True, download=True,
-                           transform=transform)
+                                    transform=transform)
         train_data.data = train_data.data[:1000]
         test_data = datasets.MNIST('../data', train=False,
-                           transform=transform)
+                                   transform=transform)
         test_data.data = test_data.data[:1000]
 
         self.train_loader = DataLoader(train_data, batch_size=500)
         self.test_loader = DataLoader(test_data, batch_size=500)
         self.model = SimpleNet()
         self.optimizer = Adam(self.model.parameters())
+
+
     def tearDown(self) -> None:
         time.sleep(0.5)
         shutil.rmtree(wandb.config["cp_base_path"], ignore_errors=True)
+
 
     def test_train(self):
         wandb.run.name = "test_train"
@@ -60,6 +61,7 @@ class Test(unittest.TestCase):
         files = os.listdir(cp_dir)
         assert len(files) == epochs, f"Expected {epochs} epoch checkpoints, got {len(files)}"
 
+
     def test_train_epoch(self):
         wandb.run.name = "test_train_epoch"
         trainer = Trainer(self.train_loader, self.test_loader, self.test_loader)
@@ -69,27 +71,53 @@ class Test(unittest.TestCase):
 
         self.optimizer.step = MagicMock()
         self.optimizer.zero_grad = MagicMock()
+
+
         class Loss():
             pass
+
+
         loss = Loss()
         trainer.loss_fn = MagicMock(return_value=loss)
         loss.backward = MagicMock()
+        loss.item = MagicMock(return_value=1.23)
 
         trainer.metrics["acc"] = Accuracy()
         trainer.metrics["acc"].update = MagicMock()
 
+        trainer._Trainer__inter_epoch_training_log = MagicMock(return_value=True)
+        trainer._Trainer__inter_epoch_validation = MagicMock()
+
+        trainer._Trainer__reset()
+        trainer._Trainer__logging_infos["end_epoch"] = 999
+        trainer._Trainer__logging_infos["running_loss"] = 0
+        trainer.device = torch.device("cpu")
         trainer._Trainer__train_epoch(self.model, self.optimizer)
 
         assert self.model.training
-        assert
+        assert self.optimizer.step.call_count == 2
+        assert self.optimizer.zero_grad.call_count == 2
+        assert trainer.loss_fn.call_count == 2
+        assert loss.backward.call_count == 2
+        assert trainer.metrics["acc"].update.call_count == 2
+        assert trainer._Trainer__inter_epoch_training_log.call_count == 2
+        assert trainer._Trainer__inter_epoch_validation.call_count == 2
+        assert trainer._Trainer__logging_infos["running_loss"] == 2.46
 
+    def test_dry_run(self):
+        wandb.run.name = "test_dry_run"
 
     def test_train_logging(self):
         wandb.run.name = "test_train_logging"
+
+
     def test_validation(self):
         wandb.run.name = "test_validation"
+
+
     def test_cleanup(self):
         wandb.run.name = "test_cleanup"
+
 
     def test_warm_start(self):
         wandb.run.name = "test_warm_start"
