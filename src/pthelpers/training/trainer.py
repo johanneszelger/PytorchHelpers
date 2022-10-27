830f8a79f8373014e8a2d391e82d8bcb7982caae
zelger.johannes@gmail.com <zelger.johannes@gmail.com>
2022-10-27 12:07:12 +0100

    fixed test metric iteration

--- src/pthelpers/training/trainer.py
+++ src/pthelpers/training/trainer.py
@@ -196,7 +196,7 @@ class Trainer:
                 output = model(data)
                 test_loss += self.loss_fn(output, target).item()
                 pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-                for name, metric in metrics:
+                for name, metric in metrics.items():
                     metric.compute(output, target.int())
 
         test_loss /= len(test_loader)
