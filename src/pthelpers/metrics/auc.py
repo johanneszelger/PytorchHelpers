ca2c9a332a64cc7801c76555fb3fd61b3fff62ee
Johannes <zelger.johannes@gmail.com>
2023-01-25 19:52:11 +0100

    added auc metric

--- src/pthelpers/metrics/auc.py
+++ src/pthelpers/metrics/auc.py
@@ -1,5 +1,5 @@
 import torch
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, roc_curve
 from torch import Tensor
 from torchmetrics import Metric, Accuracy
 
@@ -10,7 +10,8 @@ class AUC(Metric):
     def __init__(self, label_value: int):
         super().__init__()
         self.label_value = label_value
-        self.add_state("count", default=torch.Tensor(), dist_reduce_fx='mean')
+        self.add_state("targets", default=torch.Tensor(), dist_reduce_fx='mean')
+        self.add_state("preds", default=torch.Tensor(), dist_reduce_fx='mean')
         self.panel = "data"
 
 
@@ -22,15 +23,17 @@ class AUC(Metric):
             preds: Predictions from model (logits, probabilities, or labels)
             target: Ground truth labels
         """
-        self.append(roc_auc_score(target[:, self.label_value], preds[:, self.label_value]))
+        self.target.append(target[:, self.label_value])
+        self.preds.append(preds[:, self.label_value])
 
 
     def compute(self) -> Tensor:
         """Computes accuracy based on inputs passed in to ``update`` previously."""
-        return torch.mean(self.count)
+        return roc_auc_score(self.targets, self.preds)
 
 
 if __name__ == '__main__':
     from torchmetrics.utilities import check_forward_full_state_property
 
-    check_forward_full_state_property(LabelCount, {"label_value": 0}, {"preds": [], "target": torch.tensor([0, 0, 1, 1, 0])})
+    check_forward_full_state_property(AUC, {"label_value": 0}, {"preds": torch.tensor([[0.25, 0.12, 0.11, 0.8]]),
+                                                                "target": torch.tensor([[0, 0, 0, 1]])})
