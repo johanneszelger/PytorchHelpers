71458896884e2b9e44c0a277331b2f496d8fe637
Johannes <zelger.johannes@gmail.com>
2023-02-06 18:24:41 +0000

    fixed cm

--- src/pthelpers/metrics/confusion_matrix.py
+++ src/pthelpers/metrics/confusion_matrix.py
@@ -8,14 +8,13 @@ from src.pthelpers.logging.confusion_matrix import confusion_matrix
 class ConfusionMatrix(Metric):
     full_state_update: bool = False
 
-
     def __init__(self, name_prefix: str, class_names, label_value: int):
         super().__init__()
         self.name_prefix = name_prefix
         self.class_names = class_names
         self.label_value = label_value
-        self.add_state("count", default=torch.zeros(1), dist_reduce_fx='sum')
-        self.add_state("count", default=torch.zeros(1), dist_reduce_fx='sum')
+        self.add_state("targets", default=torch.zeros(1), dist_reduce_fx='sum')
+        self.add_state("preds", default=torch.zeros(1), dist_reduce_fx='sum')
 
 
     def update(self, preds: Tensor, target: Tensor) -> None:  # type: ignore
@@ -27,13 +26,17 @@ class ConfusionMatrix(Metric):
             target: Ground truth labels
         """
         if target.ndim == 1:
-            self.count += (target == self.label_value).sum()
+            self.targets += (target == self.label_value).sum()
         else:
-            self.count += target[:, self.label_value].sum()
+            self.targets += target[:, self.label_value].sum()
+        if preds.ndim == 1:
+            self.preds += (target == self.label_value).sum()
+        else:
+            self.preds += target[:, self.label_value].sum()
 
 
     def compute(self) -> Tensor:
         """Computes accuracy based on inputs passed in to ``update`` previously."""
-        confusion_matrix(None, self.name + "_cm_table" + self.name, self.true, self.preds, self.classnames, self.name_prefix + "_cm")
+        confusion_matrix(None, self.name + "_cm_table" + self.name, self.targets, self.preds, self.classnames, self.name_prefix + "_cm")
         return None
 
